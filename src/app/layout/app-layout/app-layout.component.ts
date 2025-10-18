607de29ae710bd2554bb5c3503e7afd4e528b668
Karim Ali <kareemaly.2004@gmail.com>
2025-10-18 23:03:25 +0300

    app-layout is built

---
+++ src/app/layout/app-layout/app-layout.component.ts
@@ -0,0 +1,23 @@
+import  {Component} from '@angular/core';
+import {RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-layout',
+    templateUrl: './app-layout.component.html',
+    styleUrl:'./app-layout.component.css',
+    imports:[RouterOutlet],
+})
+
+export class AppLayoutComponent  {
+
+    navigationItems = [
+        {label:'Home',icon:"fa-solid fa-house",active:true},
+        {label:'Search',icon:"fa-solid fa-magnifying-glass",active:false},
+        {label:'Notifications',icon:"fa-regular fa-bell",active:false},
+        {label:'Bookmarks',icon:"fa-regular fa-bookmark",active:false}
+    ]
+    userItems = [
+        {label:'profile',icon:"fa-regular fa-user",active:false},
+        {label:'Settings',icon:"fa-solid fa-gear",active:false}
+    ]
+}
